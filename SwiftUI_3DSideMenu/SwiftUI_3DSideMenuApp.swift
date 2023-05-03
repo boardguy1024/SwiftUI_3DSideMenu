8ccbf816a5ce44d04e420ca0db5d380309992879
パク <ptn-kyungsuk.park@zozo.com>
2023-05-03 13:17:05 +0900

    Initial Commit

---
+++ SwiftUI_3DSideMenu/SwiftUI_3DSideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_3DSideMenuApp.swift
+//  SwiftUI_3DSideMenu
+//
+//  Created by パク on 2023/05/03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_3DSideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
